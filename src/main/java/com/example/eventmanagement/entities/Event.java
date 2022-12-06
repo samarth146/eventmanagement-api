28b08f6f9580424d89ef2273ff46643e37f80d7d
157420 <157420@93W1P13.ap.corp.arrow.com>
2022-12-06 15:11:33 +0530

    Added changes for started field to come first.

--- src/main/java/com/example/eventmanagement/entities/Event.java
+++ src/main/java/com/example/eventmanagement/entities/Event.java
@@ -18,7 +18,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonPropertyOrder({"starrte"})
+@JsonPropertyOrder({"started"})
 @Entity
 public class Event extends AbstractEntity {
 
