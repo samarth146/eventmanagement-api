6eacc813119be5cd09260265a4d6d89ec9a9c5ba
157420 <157420@93W1P13.ap.corp.arrow.com>
2022-12-06 18:04:49 +0530

    Created new custom api's

--- src/main/java/com/example/eventmanagement/controllers/CheckInController.java
+++ src/main/java/com/example/eventmanagement/controllers/CheckInController.java
@@ -6,19 +6,26 @@ import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.eventmanagement.controllers.exceptioins.AlreadyCheckedInException;
 import com.example.eventmanagement.entities.Participant;
 import com.example.eventmanagement.entities.Venue;
 import com.example.eventmanagement.repos.ParticipantRepository;
 import com.example.eventmanagement.repos.VenueRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
-@RepositoryRestController
+@RestController
 @RequestMapping("/events")
 public class CheckInController {
 
@@ -46,23 +53,25 @@ public class CheckInController {
 	}
 	
 	@GetMapping("/getParticipant/{emailId}")
-	public Participant getParticipantByEmailId(@PathVariable String emailId,PersistentEntityResourceAssembler assembler) {
+	public ResponseEntity<Participant> getParticipantByEmailId(@PathVariable String emailId,PersistentEntityResourceAssembler assembler) {
 		//fetching participant data based on email Id
 		Participant participant =participantRepository.findByEmail(emailId);
 		if (participant == null) {
 			return null;
 		}
-		return participant;
+		return new ResponseEntity<Participant>(participant,HttpStatus.OK);
 		
 	}
-	@GetMapping("/getVenueByPostalCode/{postCode}")
-	public Venue getVenureByPostalCode(@PathVariable String postCode,PersistentEntityResourceAssembler assembler) {
-		//fetching Venue data based on postal code
-		Venue venue =venueRepository.findByPostalCode(postCode);
-		if (venue == null) {
-			return null;
+	@RequestMapping(value="/getVenueByPostalCode/{postCode}", method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Venue> getVenueByPostalCode(@PathVariable String postCode) {
+		// fetching Venue data based on postal code
+		Venue venue = venueRepository.findByPostalCode(postCode);
+		try {
+			return new ResponseEntity<Venue>(venue,HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		return venue;
-		
+
+		return null;
 	}
 }
